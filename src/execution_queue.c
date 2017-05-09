01c75438c93e57c913b23aaf283777fe8e9b0467
Guilherme Tassinari <guitassinari@gmail.com>
2017-05-09 16:51:15 -0300

    missing tests

--- src/execution_queue.c
+++ src/execution_queue.c
@@ -2,10 +2,28 @@
 
 TCB_t * currentThreadOnExecution;
 
+/*
+  Função que retira a atual thread da fila de execução e retorna um ponteiro
+  para a sua estrutura TCB.
+  Caso haja algum erro, retorna -1
+*/
 TCB_t * removeThreadFromExecutionQueue(){
-
+  if(currentThreadOnExecution == NULL){ //Se não existe uma thread atualmente.
+    return -1; // Retorna erro
+  }
+  TCB_t thread = currentThreadOnExecution; //Salva a referencia à atual thread
+  currentThreadOnExecution = NULL; //Remove a thread atual
+  return thread; //Retorna a referencia da thread retirada
 }
 
+/*
+  Função que adiciona uma thread na fila de execução.
+  Caso haja algum erro, retorna -1;
+*/
 void addThreadToExecutionQueue(TCB_t * thread){
-
+  if(currentThreadOnExecution != NULL){ //Se ja existe uma thread
+    return -1; //Retornar erro
+  }
+  currentThreadOnExecution = thread; //Seta a nova thread
+  return 0; //Retornar sucesso
 }
--- src/short_scheduler.c
+++ src/short_scheduler.c
@@ -1,14 +1,22 @@
 #include "../include/cdata.g"
 #include "../include/execution_queue.h"
 // #include "../ready_queue.h"
+#include <ucontext.h>
+
 
 void executeNextThread(){
-  TCB_t * oldExecutingThread;
-  // TODO : remover thread atual da fila de execução e coloca-la na fila de aptos
-  // if(oldExecutingThread = removeThreadFromExecutionQueue()){
-  //   addThreadToReadyQueye(oldExecutingThread);
-  // }
-  TCB_t * nextThread = getNextThreadToExecute();
-  //TODO : Retirar thread da fila de aptos, se necessário! (Depende da arquitetura).
-  addThreadToExecutionQueue(nextThread);
+  TCB_t * oldExecutingThread = removeThreadFromExecutionQueue(); //Remove a thread atual da fila de execução
+  TCB_t * nextThread = getNextThreadToExecute(); //Pede a próxima thread a ser executada para a fila de aptos
+  removeThreadFromReadyQueue(nextThread->tid) //Remove a próxima thread da fila de aptos
+  addThreadToExecutionQueue(nextThread); //Adiciona a nova thread na fila de execução
+  if(oldExecutingThread != -1){ //Se havia uma thread em execução
+    addThreadToReadyQueue(oldExecutingThread); //Adiciona a antiga thread na fila de aptos
+    getContext(oldExecutingThread->context); //Salva o contexto atual na antiga thread
+  }
+  int * isReturningFromExecution = malloc(sizeof(int)); //Flag para saber se estamos retornando do contexto salv na antiga thread
+  *isReturningFromExecution = 0;
+  if(!isReturningFromExecution){ //Se não estiver voltando o contexto
+    *isReturningFromExecution = 1; //Seta a flag para que, na próxima vez, saibamos que estamos voltando do contexto
+    setContext(nextThread->context); //Executa o contexto da próxima thread
+  }
 }
