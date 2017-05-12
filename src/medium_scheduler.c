f3ac2428dba19a746115ba30131f1085a4a1cb61
dkmisterv <vilmar.neto@gmail.com>
2017-05-11 23:27:55 -0300

    Pseudocode for medium scheduler and gitignore added

---
+++ src/medium_scheduler.c
@@ -0,0 +1,62 @@
+// Localizar em  Cthreads/src/
+
+#include "../include/cdata.h"
+#include <ucontext.h>
+#include <stdlib.h>
+#include <stddef.h>
+/*
+Escalonador de Medio Prazo usara as seguintes filas:
+
+execution_queue
+blocked_queue
+ready_queue
+
+
+*/
+/*
+int BlockCurrentThread():
+Funcao que remove a thread atual da fila de execucao
+E a coloca na fila de bloqueados
+
+Existem dois casos:
+
+1 - Se tiver uma thread na fila de executando
+0 - Caso nao houver uma thread na fila de execucao
+
+
+*/
+int BlockCurrentThread()
+{
+  TCB_t * currentThread = removeFirstThreadFromExecutionQueue();
+  addThreadtoBlockedQueue(currentThread);
+  getcontext(&currentThread->context);
+  if(currentThread == NULL){
+    return 0;
+  }
+  setcontext(&currentThread->context);
+  return 1;
+
+};
+/*
+void UnblockThread(int tid):
+
+Funcao que retira a primeira thread da fila de bloqueados
+E a coloca na fila de aptos
+
+Existem dois casos:
+
+1 - Se tiver uma thread na fila de bloqueados
+0 - Caso nao houver uma thread na fila de bloqueio
+*/
+
+int UnblockThread(int tid)
+{
+  TCB_t * firstThread = removeThreadFromBlockedQueue(tid);
+  addThreadtoReadyQueue(firstThread);
+  getcontext(&firstThread->context);
+  if(firstThread == NULL){
+    return 0;
+  }
+  setcontext(&firstThread->context);
+  return 1;
+};
