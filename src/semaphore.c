20903a22590c25dd769085f28e1e857a7e81cd81
Guilherme Tassinari <guitassinari@gmail.com>
2017-05-16 21:45:16 -0300

    first commig

---
+++ src/semaphore.c
@@ -0,0 +1,20 @@
+
+int newSemaphore(c_sem * semaphore){
+
+}
+
+int waitOnThread(int tid){
+
+}
+
+int waitOnSemaphore(c_sem * semaphore){
+
+}
+
+int releaseSemaphore(c_sem semaphore){
+
+}
+
+int releaseThread(int tid){
+
+}
