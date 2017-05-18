c5873ab4fde36ff1431629eab80de73bf88e36ba
Guilherme Tassinari <guitassinari@gmail.com>
2017-05-18 20:56:50 -0300

    adds state to thread on execution

--- src/execution_queue.c
+++ src/execution_queue.c
@@ -23,6 +23,7 @@ int addThreadToExecutionQueue(TCB_t * thread){
     return -1; //Retornar erro
   }
   currentThreadOnExecution = thread; //Seta a nova thread
+  currentThreadOnExecution->state = 2; //EXECUÇÂO
   return 0; //Retornar sucesso
 }
 
