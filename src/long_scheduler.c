6e2d3fa720d50131426c03028b0053280e6d75b1
Guilherme Tassinari <guitassinari@gmail.com>
2017-05-21 20:14:37 -0300

    better code

--- src/long_scheduler.c
+++ src/long_scheduler.c
@@ -1,5 +1,6 @@
-#include "../include/long_scheduler.h"
-
+#include "../include/execution_queue.h"
+#include "../include/ready_queue.h"
+#include <ucontext.h>
 
 
 /* escalonador de longo prazo vai usar as seguintes filas:
@@ -12,22 +13,33 @@ int Tid = 0;
 
 int newTid()
 {
-  Tid = Tid + 1;
-  
-  return Tid;
+  int currentTid = Tid;
+  Tid++;
+  return currentTid;
+}
+
+
+void endThread(){
+  TCB_t currentThread = removeThreadFromExecutionQueue();
+  releaseThread(currentThread->tid);
+  free(currentThread);
+  executeNextThread();
 }
 
 void createNewThread((void*) context, int prio){
   TCB_t * newThread = malloc(sizeof(TCB_t));
-  newThread->tid = newTid(); 
-  ucontext_t threadContext;
-  getcontext(&threadContext);
-  makecontext(&threadContext, context);
-  threadContext.uc_link = releaseThread(newThread->tid);
-  char stack[256]
-  threadContext.stack.ss_sp = stack;
-  threadContext.stack.ss_size = sizeof(stack);
-  thread.context = threadContext;
+  newThread->tid = newTid();
+  thread.context = newContext(context);
   addThreadToReadyQueue(thread);
 }
 
+ucontext_t newContext((void*)context){
+  ucontext_t thisNewContext;
+  getcontext(&thisNewContext);
+  makecontext(&thisNewContext, context);
+  char stack[256]
+  thisNewContext.stack.ss_sp = stack;
+  thisNewContext.stack.ss_size = sizeof(stack);
+  thisNewContext.uc_link = endThread;
+  return thisNewContext;
+}
