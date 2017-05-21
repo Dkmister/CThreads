a0ad64bf45f3f0275e2fce553e6be02b0ef44f4d
Guilherme Tassinari <guitassinari@gmail.com>
2017-05-21 19:31:30 -0300

    first commit

---
+++ src/interface.c
@@ -0,0 +1,44 @@
+#include "../include/cdata.h"
+#include "../include/cthread.h"
+
+#include "../include/errors.h"
+#include "../include/semaphore.h"
+
+#include "../include/ready_queue.h"
+#include "../include/execution_queue.h"
+
+#include "../include/short_scheduler.h"
+#include "../include/medium_scheduler.h"
+
+int ccreate (void* (*start)(void*), void *arg, int prio){
+
+}
+
+int csetprio(int tid, int prio){
+
+}
+
+int cyield(void){
+  
+}
+
+int cjoin(int tid){
+  return waitOnThread(tid);
+}
+
+int csem_init(csem_t *sem, int count){
+  return newSemaphore(sem, count);
+}
+
+int cwait(csem_t *sem){
+  return waitOnSemaphore(sem);
+}
+
+int csignal(csem_t *sem){
+  return releaseSemaphore(sem);
+}
+
+int cidentify (char *name, int size){
+  strcpy(name, "231060 G. Tassinari\nV. Neto\nJ. Nakamura\n\0");
+  return SUCCESS;
+}
